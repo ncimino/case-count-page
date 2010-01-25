6777cd83b98789a092f87dc2dadb2e207b09a8df
Nik.Cimino@gmail.com <Nik.Cimino@gmail.com@05f8243a-d188-11de-9e88-ff79fe1e0c1e>
2010-01-25 05:52:30 +0000

    Changed the queue schedule so it doesn't show user as busy.  Added some code for shared calendar to support queue schedule, but it isn't working correctly.

--- shared_calendar.php
+++ shared_calendar.php
@@ -1,32 +1,51 @@
 <?php
 include_once("./include/includes.php");
 DB_CONNECT($con);
+
 $site_name = SITE_NAME($_GET['calendar_page'],$con);
 $file_name = preg_replace('/ /', '', $site_name);
 
 header("Content-Type: text/Calendar");
 header("Content-Disposition: inline; filename=".$file_name.".ics");
 
-BUILD_VCALENDAR_HEADER($cal_file,-7,'ical','Phone Shifts');
+$phone_page = mysql_fetch_array(mysql_query("SELECT siteID FROM Sites Where SiteName = 'phoneshift'",$con));
+
+BUILD_VCALENDAR_HEADER($cal_file,-7,'ical',$site_name);
 
 $current_time = time(); // Time is PST, but doesn't matter as this is just used to determine the current week
 $last_week = DETERMINE_WEEK($current_time-7*24*60*60);
 $next_week = DETERMINE_WEEK($current_time+7*24*60*60);
 
+if($_GET['calendar_page'] == $phone_page['siteID'])
 BUILD_PHONE_SCHEDULE_ARRAY($schedule,$last_week['Monday'],$next_week['Friday'],$_GET['calendar_page'],$con);
+else
+BUILD_QUEUE_SCHEDULE_ARRAY($schedule,$last_week['Monday'],$next_week['Friday'],$_GET['calendar_page'],$con);
 
 $current = gmdate('Ymd\THis',$current_time);
 
 $from = MAIN_EMAILS_FROM;
 
 // Build all events for each of the different of the different events
-foreach ($schedule as $date)
+if($_GET['calendar_page'] == $phone_page['siteID'])
 {
-  foreach ($date as $userID)
+  foreach ($schedule as $date)
   {
-    foreach ($userID as $shift)
+    foreach ($date as $userID)
     {
-      BUILD_VEVENT($cal_file,$from,$shift,'','ical');
+      foreach ($userID as $shift)
+      {
+        BUILD_VEVENT($cal_file,$from,$shift,'','ical');
+      }
+    }
+  }
+}
+else
+{
+  foreach ($schedule as $userID => $userID_array)
+  {
+    foreach ($userID_array as $shift)
+    {
+      BUILD_VEVENT($cal_file,$from,$shift,'','ical');;
     }
   }
 }
